20737975547de48dbd085fabb03357d59cc142af
danwolf <2dawolf@gmail.com>
2014-02-11 14:36:02 -0500

    csrf_protection still has to be disabled for site to not throw errors.

--- application/config/config.php
+++ application/config/config.php
@@ -295,7 +295,7 @@ $config['global_xss_filtering'] = TRUE;
 | 'csrf_cookie_name' = The cookie name
 | 'csrf_expire' = The number in seconds the token should expire.
 */
-$config['csrf_protection'] = TRUE;
+$config['csrf_protection'] = FALSE;
 $config['csrf_token_name'] = 'magic_token_of_love';
 $config['csrf_cookie_name'] = 'magic_token_of_giving';
 $config['csrf_expire'] = 7200;
