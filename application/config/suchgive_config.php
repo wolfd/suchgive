64af1fee5f4c4d45c5dddbea4494c04211ae2741
wolf <wolf@ccs.neu.edu>
2014-02-11 12:45:19 -0500

    Put sensitive constants into suchgive_config.php, which can be modified with correct data when on box.

---
+++ application/config/suchgive_config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Suchgive configuration file to fill with sensitive information
+ * User: wolf <wolf@ccs.neu.edu>
+ * Date: 2/11/14
+ * Time: 12:14 PM
+ */
+
+define('CODEIGNITER_MODE', 'development'); //or testing or production
+define('CONFIG_ENCRYPTION_KEY', '');
+define('DATABASE_DEFAULT_USERNAME', '');
+define('DATABASE_DEFAULT_PASSWORD', '');
+define('RECAPTCHA_PRIVATE_KEY', '');
+define('DOGECOIND_RPC_URL', 'http://user:pass@127.0.0.1:22555');
