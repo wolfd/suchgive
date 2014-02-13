1bbac94ef4b59c1e667a6d1e54918f03959159d9
danwolf <2dawolf@gmail.com>
2014-02-13 10:14:48 -0500

    Added rest of cssSandpaper. Updated more css transforms for the new test page.

--- application/views/moon.php
+++ application/views/moon.php
@@ -1,15 +1,18 @@
-
 <div></div>
-<script src="/js/cssSandpaper.js"></script>
+<script type="text/javascript" src="/js/EventHelpers.js"></script>
+<script type="text/javascript" src="/js/cssQuery-p.js"></script>
+<script type="text/javascript" src="/js/jcoglan.com/sylvester.js"></script>
+<script type="text/javascript" src="/js/cssSandpaper.js"></script>
 <script type="application/javascript">
 </script>
-<div class="moon-scene">
-
+<div class="moon-scene visible-desktop visible-tablet">
     <div class="the-moon">
         <div class="reward-banner">
             <h2 class="reward-banner-text">REWARD</h2>
+            <div class="no-size"><div class="spaceship"></div></div>
             <h2 class="reward-banner-text">MONIES</h2>
         </div>
+
     </div>
     <div class="the-earth"></div>
 </div>
